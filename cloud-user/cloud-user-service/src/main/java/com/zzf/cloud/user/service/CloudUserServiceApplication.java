92c6eb162301452254f8e0beb84388c03facf054
lronMan <zhangxin@zhaojiafang.com>
2021-04-28 13:48:27 +0800

    初始化

---
+++ cloud-user/cloud-user-service/src/main/java/com/zzf/cloud/user/service/CloudUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zzf.cloud.user.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudUserServiceApplication.class, args);
+    }
+
+}
